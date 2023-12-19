ee365d6bd14232838acc294fbef2a586ea7c4c9c
kkang <kkang104@daum.net>
2023-12-19 17:30:36 +0900

    a

--- ch6/lab6-client-data/app.js
+++ ch6/lab6-client-data/app.js
@@ -65,6 +65,65 @@ app.get('/query2', (req, res) => {
   `)
 })
 
+//test3........ form body..,. 
+//예전에는 body-parser를 별도로 설치했어야 하는데..
+//express 에서 자체적으로 지원함으로 설치하지 않아도 ... 
+//extended 옵션, express 의 바디 파서가 내부적으로 이용하는 모듈이 여러개 준비되어 있는데
+//QS 라는 모듈을 이용해서 바디를 파싱해라... 
+//거의 대부분 QS 모듈을 이용
+app.use(express.urlencoded({extended: true}))
+app.post('/form', (req, res) => {
+  let ide = req.body.ide
+  console.dir(ide)
+  let ideResult = ''
+  if(ide && Array.isArray(ide)){
+    ide.forEach((element) => {
+      ideResult += element + ','
+    })
+  }else {
+    ideResult = ide
+  }
+
+  let car2 = req.body.cars2
+  let car2Result = ''
+  if(car2 && Array.isArray(car2)){
+    car2.forEach((element) => {
+      car2Result += element + ','
+    })
+  }else {
+    car2Result = car2
+  }
+
+  res.send(`
+    입력하신 데이터는, 
+    name=${req.body.name},
+    apple=${req.body.apple},
+    peach=${req.body.peach},
+    ide=${ideResult},
+    language=${req.body.language},
+    car1=${req.body.cars1},
+    car2=${car2Result}
+  `)
+})
+
+//test4... ajax json data.... 
+//json 미들웨어, express 자체에서 제공..
+//js 에서 json 은 객체, 그런데 네트워크 전송시에는 문자열..
+//json 객체 <-> 문자열
+//JSON.parse(문자열)
+//JSON.stringify()
+//express 의 json 미들웨어가 자체적으로 네트웍상에서 넘어오는 json 문자열을 알아서 json 객체로 변형
+app.use(express.json())
+app.post('/json1', (req, res) => {
+  res.send(`json data : name=${req.body.name}, score=${req.body.score}`)
+})
+
+//test5... ajax, 일반문자열.. 
+var bodyParser = require('body-parser')
+app.use(bodyParser.text())//일반 문자열이다.. 
+app.post('/string1', (req, res) => {
+  res.send(`string data : ${req.body}`)
+})
 
 
 app.listen(app.get('port'), ()=>{
