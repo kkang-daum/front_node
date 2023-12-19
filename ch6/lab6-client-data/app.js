de222c0ff8507f5951402e2489fb2dde2e05ddbc
kkang <kkang104@daum.net>
2023-12-19 16:59:06 +0900

    a

--- ch6/lab6-client-data/app.js
+++ ch6/lab6-client-data/app.js
@@ -19,6 +19,52 @@ app.get('/name/:name/data/:data', (req, res) => {
 })
 //http://localhost:3000/index.html
 
+//test2...... query(search) 문자열 획득.. 
+//url ? 뒤에 등록되어 전달되는 데이터.. 
+//url 이 request header 정보임으로.. query 문자열은 body 데이터는 아니고.. 
+//header 데이터.. 
+app.get('/query1', (req, res) => {
+  res.send(`
+  query1 test: name=${req.query.name}, addr=${req.query.addr}, age=${req.query.age}`)
+})
+
+app.get('/query2', (req, res) => {
+  let ide = req.query.ide
+  console.dir(ide)
+  //ide 로 데이터가 안넘어 올수도 있고(유저가 선택 안했다면)
+  //ide 로 데이터가 하나만 넘어올수도 있고.. (유저가 하나만 선택하면..)
+  //ide 로 데이터가 여러개 넘어올수도 있고.. (유저가 여러개 선택하면.. ) ==>배열로..
+  let ideResult = ''
+  if(ide && Array.isArray(ide)){
+    ide.forEach((element) => {
+      ideResult += element + ','
+    })
+  }else {
+    ideResult = ide
+  }
+
+  let car2 = req.query.cars2
+  let car2Result = ''
+  if(car2 && Array.isArray(car2)){
+    car2.forEach((element) => {
+      car2Result += element + ','
+    })
+  }else {
+    car2Result = car2
+  }
+
+  res.send(`
+    입력하신 데이터는, 
+    name=${req.query.name},
+    apple=${req.query.apple},
+    peach=${req.query.peach},
+    ide=${ideResult},
+    language=${req.query.language},
+    car1=${req.query.cars1},
+    car2=${car2Result}
+  `)
+})
+
 
 
 app.listen(app.get('port'), ()=>{
