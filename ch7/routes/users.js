081821f1788c53b498d047220a8a84a112addc09
kkang <kkang104@daum.net>
2023-12-20 17:48:59 +0900

    a

---
+++ ch7/routes/users.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const User = require('../models/user')
+const Comment = require('../models/comment')
+
+const router = express.Router()
+
+router.route('/')
+  .get(async (req, res, next)=>{
+    try{
+      const users = await User.findAll()
+    }catch(err){
+      console.log(err)
+      next(err)
+    }
+  })
+  .post(async (req, res, next) => {
+    try{
+      const user = await User.create({
+        name: req.body.name,
+        age: req.body.age,
+        married: req.body.married
+      })
+      res.status(200).json(user)
+    }catch(err){
+      console.log(err)
+      next(err)
+    }
+  })
+
+  //특정 유저가 쓴 글만 보기 위해서.. 
+router.get('/:id/comments', async (req, res, next) => {
+  try{
+    const comments = await Comment.findAll({
+      include: {
+        model:User,
+        where: {id:req.params.id}
+      }
+    })
+    res.json(comments)
+  }catch(err){
+    console.log(err)
+    next(err)
+  }
+})
+
+module.exports = router
